392d0562006bddfb4ec03de97331dc4d13774bae
Shostok <ivanchechutkov@gmail.com>
2025-02-18 16:45:41 +0300

    Added slider, adeed tablet adaptive.

--- script.js
+++ script.js
@@ -83,3 +83,83 @@ navHeader.addEventListener('click', function (event) {
 // 	})
 // }
 // SMOOTH SCROLL END
+
+// let position = 0
+// const slidesToShow = 1
+// const slidesToScroll = 1
+// const container = document.querySelector('.slider-container')
+// const track = document.querySelector('.slider-track')
+// const btnPrev = document.querySelector('.arrow_slider_left')
+// const btnNext = document.querySelector('.arrow_slider_right')
+// const items = document.querySelectorAll('.pet-info_column')
+// const itemsCount = items.length
+
+// items.forEach(item => {
+// 	item.style.minWidth = `${itemWidth}px`
+// })
+
+// btnNext.addEventListener('click', () => {
+// 	const itemsLeft =
+// 		itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth
+// 	position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth
+// 	setPosition()
+// 	checkBtns()
+// })
+
+// btnPrev.addEventListener('click', () => {
+// 	const itemsLeft = Math.abs(position) / itemWidth
+// 	position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth
+// 	setPosition()
+// 	checkBtns()
+// })
+
+// const setPosition = () => {
+// 	track.style.transform = `translateX(${position}px)`
+// }
+
+// const checkBtns = () => {
+// 	btnPrev.disabled = position === 0
+// 	btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth
+// }
+
+// setItemsWidth()
+// checkBtns()
+
+const container = document.querySelector('.slider-container')
+const track = document.querySelector('.slider-track')
+const arrowLeft = document.querySelector('.arrow_slider_left')
+const arrowRight = document.querySelector('.arrow_slider_right')
+const slides = document.querySelectorAll('.pet-info_column')
+
+let currentSlideIndex = 0
+
+function showSlide() {
+	slides[currentSlideIndex].classList.add('block')
+}
+
+function hideSlide() {
+	slides[currentSlideIndex].classList.remove('block')
+}
+
+function nextSlide() {
+	hideSlide()
+	currentSlideIndex++
+	if (currentSlideIndex > slides.length - 1) {
+		currentSlideIndex = 0
+	}
+	showSlide()
+}
+
+function previousSlide() {
+	hideSlide()
+	currentSlideIndex--
+	if (currentSlideIndex < 0) {
+		currentSlideIndex = slides.length - 1
+	}
+	showSlide()
+}
+
+showSlide()
+
+arrowRight.addEventListener('click', nextSlide)
+arrowLeft.addEventListener('click', previousSlide)
